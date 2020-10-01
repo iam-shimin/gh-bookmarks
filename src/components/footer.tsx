3708c8d00d9d960b259998ec89d1efe43b7c985a
shimin mathew <simplyshimin@gmail.com>
2020-10-01 17:57:56 +0530

    add footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import 'style/footer.css';
+
+export default function Footer() {
+	return (
+		<footer>
+			<span>Github Bookmarks</span>
+			<div>
+				<a href="#api">API Reference</a>
+				<a href="#repo">Fork this repo</a>
+			</div>
+		</footer>
+	)
+}
