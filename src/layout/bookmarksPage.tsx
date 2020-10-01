e828d1618336b41de861ee10a79ff0267aaf7c4a
shimin mathew <simplyshimin@gmail.com>
2020-10-01 16:53:48 +0530

    add overall layout

---
+++ src/layout/bookmarksPage.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import Header from 'components/header';
+import TitleBar, {Title, NewButton} from 'components/titleBar';
+import SearchBar from 'components/searchBar';
+import BookmarkCard from 'components/card/bookmarkCard';
+import Footer from 'components/footer';
+
+import AlertBox from 'components/alert';
+import Container from 'components/container';
+
+export default function BookmarksPage() {
+	return (
+		<>
+			<BookmarksHeader />
+			<Container>
+				<AlertBox />
+				<BookmarkCard />
+				<BookmarkCard />
+				<BookmarkCard />
+				<BookmarkCard />
+			</Container>
+			<Footer />
+		</>
+	)
+}
+
+function BookmarksHeader() {
+	return (
+		<Header>
+			<TitleBar>
+				<Title text="Bookmarks" />
+				<NewButton />
+			</TitleBar>
+			<SearchBar />
+		</Header>
+	)
+}
---
+++ src/layout/discoverPage.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import Header from 'components/header';
+import TitleBar, { Title, BackButton } from 'components/titleBar';
+import SearchBar from 'components/searchBar';
+import Container from 'components/container';
+import RepoCard from 'components/card/bookmarkCard';
+import Footer from 'components/footer';
+
+export default function DiscoverPage() {
+	return (
+		<>
+			<DiscoverHeader />
+			<Container>
+				<RepoCard />
+				<RepoCard />
+				<RepoCard />
+				<RepoCard />
+			</Container>
+			<Footer />
+		</>
+	)
+}
+
+function DiscoverHeader() {
+	return (
+		<Header>
+			<TitleBar>
+				<BackButton />
+				<Title text="Discover" />
+			</TitleBar>
+			<SearchBar />
+		</Header>
+	)
+}
