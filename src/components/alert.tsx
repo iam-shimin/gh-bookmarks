e040c86fded55ef1d2291d40007729d60527c013
shimin mathew <simplyshimin@gmail.com>
2020-10-01 18:07:20 +0530

    add alert component

---
+++ src/components/alert.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import {Alert} from 'react-bootstrap';
+
+export default function AlertBox() {
+	return (
+		<Alert variant="primary">Add bookmarks from the Discover tab</Alert>
+	)
+}
