1b2edc905ff64409f0fbe0c0771431ae729a4e50
shimin mathew <simplyshimin@gmail.com>
2020-10-01 17:51:21 +0530

    add routes

---
+++ src/app/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router';
+import 'style/App.css';
+
+import BookmarksPage from 'layout/bookmarksPage';
+import DiscoverPage from 'layout/discoverPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={BookmarksPage} />
+        <Route path="/discover" component={DiscoverPage} />
+        <Redirect to="/" />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
