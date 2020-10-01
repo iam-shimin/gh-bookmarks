423c172adbdb6ce24f27c9003374c674f9a6ef34
shimin mathew <simplyshimin@gmail.com>
2020-10-01 18:30:44 +0530

    add fallback error boundary on app component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,17 +5,32 @@ import 'style/App.css';
 
 import BookmarksPage from 'layout/bookmarksPage';
 import DiscoverPage from 'layout/discoverPage';
+import { Alert } from 'react-bootstrap';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={BookmarksPage} />
-        <Route path="/discover" component={DiscoverPage} />
-        <Redirect to="/" />
-      </Switch>
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  state = {
+    error: null
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    return {error}
+  }
+
+  render() {
+    if (this.state.error) {
+      <Alert>Something went wrong. <a href="/">reload?</a></Alert>
+    }
+
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={BookmarksPage} />
+          <Route path="/discover" component={DiscoverPage} />
+          <Redirect to="/" />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
