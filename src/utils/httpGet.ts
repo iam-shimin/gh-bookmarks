a722dcfca9d6aca079a47407a94736f2b35ba003
shimin mathew <simplyshimin@gmail.com>
2020-10-02 13:06:59 +0530

    httpget returns response.data

--- src/utils/httpGet.ts
+++ src/utils/httpGet.ts
@@ -1,7 +1,9 @@
-import axios, {AxiosRequestConfig} from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
 const baseUrl = 'https://api.github.com';
 
 export default function get(url: string, options?: AxiosRequestConfig) {
-	return axios.get(`${baseUrl}/${url}`, options);
+	return axios
+		.get(`${baseUrl}/${url}`, options)
+		.then(response => response.data);
 }
