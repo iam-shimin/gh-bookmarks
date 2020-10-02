d846208797c94222fe46cae2cac287d11030bb5c
shimin mathew <simplyshimin@gmail.com>
2020-10-02 20:44:17 +0530

    add bootstrap css to app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Switch, Route, Redirect } from 'react-router';
 import 'style/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import BookmarksPage from 'layout/bookmarksPage';
 import DiscoverPage from 'layout/discoverPage';
