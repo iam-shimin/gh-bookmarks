c67e0a6b61f4887daa53e5c08c74c5e4ab5a1813
shimin mathew <simplyshimin@gmail.com>
2020-10-03 12:30:29 +0530

    add bookmark button on bookmark card

--- src/components/card/bookmarkCard.tsx
+++ src/components/card/bookmarkCard.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 import Card from './card';
 
-export default function BookmarkCard() {
+import { removeBookmark } from 'utils/storage';
+
+import 'style/bmkCard.css';
+
+export default function BookmarkCard({id}: {id: string}) {
 	return (
-		<Card>
+		<Card className="bmk-card">
 			<h1>Repo name</h1>
 			<span>Real name (to be shown if renamed)</span>
+			<Button variant="danger" className="add-bmk-btn" onClick={() => removeBookmark(id)}>Remove Bookmark</Button>
 		</Card>
 	)
 }
