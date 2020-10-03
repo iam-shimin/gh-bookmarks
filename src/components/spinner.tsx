ab3e5a4ab0534d6b1f84194f8712789768a18076
shimin mathew <simplyshimin@gmail.com>
2020-10-03 12:18:01 +0530

    add spinner

---
+++ src/components/spinner.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import BSpinner from 'react-bootstrap/Spinner';
+
+export default function Spinner() {
+	return <BSpinner animation="border" className="spinner-big" />;
+}
