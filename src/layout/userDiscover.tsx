0ace63880715536265fb40621486876c5b6aa157
shimin mathew <simplyshimin@gmail.com>
2020-10-03 22:00:54 +0530

    paginate user discover modal

--- src/layout/userDiscover.tsx
+++ src/layout/userDiscover.tsx
@@ -5,17 +5,26 @@ import { useParams, useHistory } from 'react-router-dom';
 import RepoCard from 'components/card/repoCard';
 import Spinner from 'components/spinner';
 import AlertBox from 'components/alert';
+import Pagination from 'components/pagination';
 
 import repositoryService from 'services/repos';
 
 export default function UserDiscover() {
 	const {userName} = useParams();
 	const [show, setShow] = React.useState(!!userName);
-	const [repositories, setRepositories] = React.useState<any[] | null>(null);
+	const [repositories, setRepositories] = React.useState<any>(null);
+	const [page, setPage] = React.useState<Page>({
+		current: 1,
+		next: 1,
+		last: 1
+	});
 	const history = useHistory();
 
-	const isLoading = repositories === null;
+	const [isLoading, setIsLoading] = React.useState(repositories === null);
+	const [error, setError] = React.useState<Error | null>(null);
 	const isEmptyList = !isLoading && repositories?.length === 0;
+
+	const pageToRender = page.current;
 
 	function hanldeHide() {
 		setShow(false);
@@ -23,11 +32,17 @@ export default function UserDiscover() {
 	}
 
 	React.useEffect(() => {
+		setIsLoading(true);
+		setError(null)
 		repositoryService
-			.getAllReposByUsername(userName)
-			.then(data => setRepositories(data))
-			.catch(error => console.log(error))
-	}, [userName]);
+			.getAllReposByUsername(userName, pageToRender)
+			.then(data => {
+				setRepositories(data.data);
+				setPage(p => ({...p, ...data.links}))
+			})
+			.catch(error => setError(error))
+			.finally(() => setIsLoading(false))
+	}, [userName, pageToRender]);
 	
 	return (
 		
@@ -47,8 +62,18 @@ export default function UserDiscover() {
 						? <Spinner />
 						: isEmptyList
 							? <AlertBox>This user has no public Repositories</AlertBox>
-							: repositories?.map(repo => <RepoCard data={repo} />)
+							: (
+								<>
+									{repositories?.map((repo: any) => <RepoCard key={repo.id} data={repo} />)}
+									<Pagination
+										page={page}
+										onNext={() => setPage(p => ({...p, current: p.current + 1}))}
+										onPrev={() => setPage(p => ({...p, current: p.current - 1}))} />
+								</>
+							)
 					}
+
+					{error && <AlertBox variant="danger">{error.message}</AlertBox>}
 				</Modal.Body>
 			</Modal>
 	)
