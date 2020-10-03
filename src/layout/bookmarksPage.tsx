238ab32d0cb22217f81ed79b4b9a0047a6e852a5
shimin mathew <simplyshimin@gmail.com>
2020-10-03 12:19:31 +0530

    get saved bookmarks from localStorage

--- src/layout/bookmarksPage.tsx
+++ src/layout/bookmarksPage.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import TitleBar, {Title, NewButton} from 'components/titleBar';
 import SearchBar from 'components/searchBar';
 import BookmarkCard from 'components/card/bookmarkCard';
 import Footer from 'components/footer';
-
 import AlertBox from 'components/alert';
 import Container from 'components/container';
 
+import { getBookmarks } from 'utils/storage';
+
 export default function BookmarksPage() {
+	const added = getBookmarks();
 	return (
 		<>
 			<BookmarksHeader />
 			<Container>
-				<AlertBox />
-				<BookmarkCard />
-				<BookmarkCard />
-				<BookmarkCard />
-				<BookmarkCard />
+
+				{added.length === 0
+					? <AlertBox>Bookmarks added from the Discover Page <Link to="/discover">[+ New ]</Link> will be shown here</AlertBox>
+					: added.map(bmkId => <BookmarkCard id={bmkId} />)
+				}
+				
 			</Container>
 			<Footer />
 		</>
