1b4cab512177c69491eb4c3391d5b66139a50adf
shimin mathew <simplyshimin@gmail.com>
2020-10-15 15:56:52 +0530

    add redux provider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Switch, Route, Redirect } from 'react-router';
+import { Provider } from 'react-redux';
+
 import 'style/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import store from 'store';
 import BookmarksPage from 'layout/bookmarksPage';
 import DiscoverPage from 'layout/discoverPage';
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={BookmarksPage} />
-        <Route path="/discover" component={DiscoverPage} />
-        <Redirect to="/" />
-      </Switch>
+      <Provider store={store}>
+        <Switch>
+          <Route path="/" exact component={BookmarksPage} />
+          <Route path="/discover" component={DiscoverPage} />
+          <Redirect to="/" />
+        </Switch>
+      </Provider>
     </BrowserRouter>
   );
 }
