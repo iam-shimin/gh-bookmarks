ebcd90f909e9c1cd4646d3558829cfaa88137f4d
shimin mathew <simplyshimin@gmail.com>
2020-10-15 18:21:31 +0530

    add module for persisting redux state

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,41 +1,22 @@
 const bookmarks = 'B';
 
-function load(key: string, defaultValue: string = '[]') {
-	return JSON.parse(localStorage.getItem(key) || defaultValue);
+function load(key: string) {
+	const valueAsString = localStorage.getItem(key);
+	if (valueAsString) {
+		return JSON.parse(valueAsString);
+	}
 }
 
 function dump(key: string, obj: any) {
 	localStorage.setItem(key, JSON.stringify(obj));
 }
 
-export function addBookmark(id: string, details?: BookmarkDetails) {
-	const bmks: string[] = load(bookmarks);
-	bmks.push(id);
-	dump(bookmarks, bmks);
+export default {
+	persist(state: BookmarkState) {
+		dump(bookmarks, state)
+	},
 
-	if (details) {
-		dump(id, details);
+	load() {
+		return load(bookmarks);
 	}
-}
-
-export function removeBookmark(id: string) {
-	const bmks: string[] = load(bookmarks);
-	const i = bmks.indexOf(id);
-	if (i === -1)
-		return;
-	bmks.splice(i, 1);
-	dump(bookmarks, bmks)
-	localStorage.removeItem(id);
-}
-
-export function bookmarkExist(id: string) {
-	return load(bookmarks).includes(id);
-}
-
-export function getBookmarks(): string[] {
-	return load(bookmarks);
-}
-
-export function getBookmarkDetails(id: string) {
-	return load(id);
 }
