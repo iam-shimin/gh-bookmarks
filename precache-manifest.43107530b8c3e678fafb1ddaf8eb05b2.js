431041a163c4ea698f3bbb226e549a5168cfd504
shimin mathew <simplyshimin@gmail.com>
2020-10-30 17:00:20 +0530

    Updates

---
+++ precache-manifest.43107530b8c3e678fafb1ddaf8eb05b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0a698bbc56a85da8458cbd738eb717d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f778ef54729f1a8e37f",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6280a3c7d8ea1a9783c0",
+    "url": "/static/css/main.f543a0f9.chunk.css"
+  },
+  {
+    "revision": "6f778ef54729f1a8e37f",
+    "url": "/static/js/2.73e0f205.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.73e0f205.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6280a3c7d8ea1a9783c0",
+    "url": "/static/js/main.8a772acd.chunk.js"
+  },
+  {
+    "revision": "c22573d7a896ad7fb92d",
+    "url": "/static/js/runtime-main.b61acdbe.js"
+  },
+  {
+    "revision": "f9f267908e3859b5815031d867677ccc",
+    "url": "/static/media/user-icon.f9f26790.png"
+  }
+]);
