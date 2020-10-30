8587ab07005426b2b77864908bb07cd8b4a9756a
shimin mathew <simplyshimin@gmail.com>
2020-10-30 15:43:44 +0530

    static type httpget

--- src/utils/httpGet.ts
+++ src/utils/httpGet.ts
@@ -3,7 +3,7 @@ import axios, { AxiosRequestConfig } from 'axios';
 const baseUrl = 'https://api.github.com';
 
 function getSearchParamFromLink(url: string, param = 'page') {
-	let tmp: any = url?.trim();
+	let tmp: string | null = url.trim();
 	if (tmp) {
 		tmp = (new URLSearchParams(url.substr(1, url.length - 2))).get(param);
 		if (tmp) {
@@ -18,10 +18,10 @@ function parseLinkHeader(link = '') {
 		return null;
 
 	const links = Object.fromEntries(link.split(',').map(i => i.split('; ').reverse()));
-	let next: any = getSearchParamFromLink(links["rel=\"next\""]);
-	let prev: any = getSearchParamFromLink(links["rel=\"prev\""]);
-	let first: any = getSearchParamFromLink(links["rel=\"first\""]);
-	let last: any = getSearchParamFromLink(links["rel=\"last\""]);
+	let next = getSearchParamFromLink(links["rel=\"next\""]);
+	let prev = getSearchParamFromLink(links["rel=\"prev\""]);
+	let first = getSearchParamFromLink(links["rel=\"first\""]);
+	let last = getSearchParamFromLink(links["rel=\"last\""]);
 	return {first, prev, next, last}
 }
 
