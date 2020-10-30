84868f0266b98cfc1cae4efe3bc332122f80d4bb
shimin mathew <simplyshimin@gmail.com>
2020-10-30 17:07:10 +0530

    Updates

---
+++ precache-manifest.5e74b509c85911bf14e4cae83202302d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8892b6b6cb18403e2138cae7c659f154",
+    "url": "/gh-bookmarks/index.html"
+  },
+  {
+    "revision": "6f778ef54729f1a8e37f",
+    "url": "/gh-bookmarks/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7aafec6f2b6a2a8acb52",
+    "url": "/gh-bookmarks/static/css/main.f543a0f9.chunk.css"
+  },
+  {
+    "revision": "6f778ef54729f1a8e37f",
+    "url": "/gh-bookmarks/static/js/2.73e0f205.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/gh-bookmarks/static/js/2.73e0f205.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aafec6f2b6a2a8acb52",
+    "url": "/gh-bookmarks/static/js/main.1627a74b.chunk.js"
+  },
+  {
+    "revision": "b9708923c4c456b6c609",
+    "url": "/gh-bookmarks/static/js/runtime-main.c6f8f92c.js"
+  },
+  {
+    "revision": "f9f267908e3859b5815031d867677ccc",
+    "url": "/gh-bookmarks/static/media/user-icon.f9f26790.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.43107530b8c3e678fafb1ddaf8eb05b2.js"
+  "/gh-bookmarks/precache-manifest.5e74b509c85911bf14e4cae83202302d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gh-bookmarks/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
