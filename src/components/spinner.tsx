cd2ed291df5c0ea588c572dc23b3d60dbcd85ebe
shimin mathew <simplyshimin@gmail.com>
2020-11-09 19:36:06 +0530

    place spinner on the center

--- src/components/spinner.tsx
+++ src/components/spinner.tsx
@@ -2,5 +2,17 @@ import React from 'react';
 import BSpinner from 'react-bootstrap/Spinner';
 
 export default function Spinner() {
-	return <BSpinner animation="border" className="spinner-big" />;
+
+	const wrapperStyle: React.CSSProperties = {
+		position: 'fixed',
+		top: '50%',
+		left: '50%',
+		transform: 'translate(-50%, -50%)'
+	};
+
+	return (
+		<div style={wrapperStyle}>
+			<BSpinner animation="border" className="spinner-big" />
+		</div>
+	);
 }
