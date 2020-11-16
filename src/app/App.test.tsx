37a9462289a61d85a5e34b6fc6069b82a54edacb
shimin mathew <simplyshimin@gmail.com>
2020-11-16 17:15:06 +0530

    app crash test

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  // const { getByText } = 
+test('App crash test', () => {
   render(<App />);
-  // const linkElement = getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
 });
