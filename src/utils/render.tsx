c1d044f056dd074e85e55b31c8d6480e1d1d95c2
shimin mathew <simplyshimin@gmail.com>
2020-11-17 11:55:39 +0530

    make store injectable in test render with redux

--- src/utils/render.tsx
+++ src/utils/render.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
+import { Store } from 'redux';
 import store from 'store';
 
 // Test render
 
-export default function renderWithStoreAndRouter(element: React.ReactElement) {
+export default function renderWithStoreAndRouter(
+	element: React.ReactElement,
+	injectedStore?: Store<BookmarkState, BookmarkAction>
+) {
+	const storeToBeUsed = injectedStore || store;
 	return render(
 		<MemoryRouter>
-			<Provider store={store}>{element}</Provider>
+			<Provider store={storeToBeUsed}>{element}</Provider>
 		</MemoryRouter>
 	);
 }
