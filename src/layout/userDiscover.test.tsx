267b2d08728f3cae4c07c5a0578541c10cb4ec28
shimin mathew <simplyshimin@gmail.com>
2020-11-23 01:31:54 +0530

    add test for rendering list of user repo in userdiscover modal

--- src/layout/userDiscover.test.tsx
+++ src/layout/userDiscover.test.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import renderWithStoreAndRouter from 'utils/render';
 import UserDiscover from './userDiscover';
+jest.mock('services/repos');
 
 describe('<UserDiscoverModal />', () => {
 	test('match snapshot', () => {
 		const { asFragment } = renderWithStoreAndRouter(<UserDiscover />);
 		expect(asFragment()).toMatchSnapshot();
 	});
+
+	test('open a user repo-list', async () => {
+		const testUser = 'user-a';
+		const element = (
+			<Route path="/discover/user/:userName">
+				<UserDiscover />
+			</Route>
+		);
+		const rendererOptions = {
+			routerProps: { initialEntries: [{
+				pathname: `/discover/user/${testUser}`
+			}] }
+		};
+		const { findByText, findAllByRole } = renderWithStoreAndRouter(element, rendererOptions);
+		const modalTitle = await findByText(`GitHub Repositories of ${testUser}`);
+		const result = await findAllByRole('heading');
+
+		expect(modalTitle).toBeInTheDocument();
+		expect(result.length).toEqual(2);
+	})
 });
